1e4323756aba221799ac220cfb30b1cee27170ea
unknown <you@example.com>
2024-02-10 18:14:12 +0100

    Test

---
+++ MyStack.java
@@ -0,0 +1,19 @@
+public class MyStack {
+    private int size;
+    public MyStack(int size) {
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void push(int element) {
+        if(size > 4) throw new StackOverflowError("Error");
+        ++size;
+    }
+
+    public void pop(int element) {
+        if(isEmpty()) throw new IllegalArgumentException("ArrayIndexOutOfBoundsException");
+        --size;
+    }
+}
