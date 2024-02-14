1c7f82044472c7051da16262da3f81850c496faf
Baliqis Sulaiman <151347697+bally3399@users.noreply.github.com>
2024-02-14 13:36:29 +0100

    Update SetTest.java

--- SetTest.java
+++ SetTest.java
@@ -1,21 +1,17 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SetTest {
-    private Set mySet;
 
-    @BeforeEach
-    public void Set() {
-        mySet = new Set();
-    }
     @Test
     public void testThatSetIsEmpty(){
+        Set mySet = new Set();
         assertTrue(mySet.isEmpty());
     }
     @Test
     public void addElement_setIsNotEmpty(){
+        Set mySet = new Set();
         assertTrue(mySet.isEmpty());
         mySet.add("Orange");
         assertFalse(mySet.isEmpty());
@@ -23,6 +19,7 @@ public class SetTest {
 
     @Test
     public void addTwoElementsTest(){
+        Set mySet = new Set();
         assertTrue(mySet.isEmpty());
         mySet.add("Apple");
         mySet.add("Orange");
@@ -31,6 +28,7 @@ public class SetTest {
 
     @Test
     public void addA_removeA_setIsEmptyTest(){
+        Set mySet = new Set();
         assertTrue(mySet.isEmpty());
         mySet.add("Apple");
         mySet.remove("Apple");
@@ -39,17 +37,18 @@ public class SetTest {
     }
     @Test
     public void addAB_removeAll_setIsEmptyTest(){
+        Set mySet = new Set();
         assertTrue(mySet.isEmpty());
         mySet.add("Apple");
         mySet.add("Orange");
         mySet.add("Banana");
+        mySet.removeAll(mySet);
         assertFalse(mySet.isEmpty());
-        mySet.removeAll();
-        assertTrue(mySet.isEmpty());
     }
 
     @Test
     public void addSameElement_returnOneTest(){
+        Set mySet = new Set();
         assertTrue(mySet.isEmpty());
         mySet.add("Apple");
         mySet.add("Apple");
@@ -58,6 +57,7 @@ public class SetTest {
 
     @Test
     public void addA_checkIfTheElementContainATest() {
+        Set mySet = new Set();
         assertTrue(mySet.isEmpty());
         mySet.add("Apple");
         assertTrue(mySet.contains("Apple"));
@@ -65,6 +65,7 @@ public class SetTest {
 
     @Test
     public void addXY_checkIfTheSetContainXYTest(){
+        Set mySet = new Set();
         assertTrue(mySet.isEmpty());
         mySet.add("Apple");
         mySet.add("Orange");
@@ -74,12 +75,14 @@ public class SetTest {
 
     @Test
     public void setDoesNotContainsElement(){
+        Set mySet = new Set();
         assertTrue(mySet.isEmpty());
         mySet.add("Orange");
         assertFalse(mySet.contains("Apple"));
     }
     @Test
     public void addSameElement_returnTwoTest(){
+        Set mySet = new Set();
         assertTrue(mySet.isEmpty());
         mySet.add("Apple");
         mySet.add("apple");
@@ -87,13 +90,21 @@ public class SetTest {
     }
     @Test
     public void addXY_checkIfTheSetContainAllTest(){
+        Set mySet = new Set();
         assertTrue(mySet.isEmpty());
         mySet.add("Apple");
         mySet.add("Orange");
-        assertTrue(mySet.containAll());
+        mySet.add("lemon");
+
+        Set mySet1 = new Set();
+        mySet.add("Orange");
+        mySet.add("pine_apple");
+        mySet.add("lemon");
+        assertTrue(mySet.containAll(mySet1));
     }
     @Test
     public void getSize_returnTheLengthOfTheSetTest(){
+        Set mySet = new Set();
         assertTrue(mySet.isEmpty());
         mySet.add("Orange");
         mySet.add("Apple");
@@ -103,12 +114,16 @@ public class SetTest {
 
     @Test
     public void addThreeElementTest(){
+        Set mySet = new Set();
         assertTrue(mySet.isEmpty());
         mySet.add("Orange");
         mySet.add("Apple");
         mySet.add("Lemon");
-        assertTrue(mySet.addAll());
+
+        Set mySet1 = new Set();
+        mySet.add("orange");
+        mySet.add("apple");
+        assertTrue(mySet.addAll(mySet1));
     }
 
-
-    }
+}
