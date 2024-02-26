34f058c47f8cdf40056697d4a5f85c611a2de80c
Baliqis Sulaiman <151347697+bally3399@users.noreply.github.com>
2024-02-26 10:16:47 +0100

    Update DairiesTest.java

--- DairiesTest.java
+++ DairiesTest.java
@@ -30,4 +30,37 @@ public class DairiesTest {
         dairies.remove("username", "password");
         assertEquals(0, dairies.getLength().size());
     }
+    @Test
+    public void addTwiceTest(){
+        Dairies dairies = new Dairies();
+        dairies.add("username","password");
+
+        dairies.add("username1","password1");
+        assertEquals(2, dairies.getLength().size());
+    }
+
+    @Test
+    public void addTwice_deleteOneTest(){
+        Dairies dairies = new Dairies();
+        dairies.add("username","password");
+        dairies.add("username1","password1");
+
+        dairies.remove("username", "password");
+        assertEquals(1, dairies.getLength().size());
+    }
+    @Test
+    public void findTwoUSerByName(){
+        Dairies dairies = new Dairies();
+        dairies.add("username","password");
+        dairies.add("username1","password1");
+
+        Dairy foundDairy = dairies.findByUserName("username");
+        Dairy foundDairy1 = dairies.findByUserName("username1");
+
+        assertEquals("username", foundDairy.getUsername());
+        assertEquals("username1", foundDairy1.getUsername());
+
+    }
+
+
 }
